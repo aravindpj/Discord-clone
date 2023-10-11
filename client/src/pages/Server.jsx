4a36cd704514112cdf93250c7876a8cad2f88fc0
Aravind pj <aravindjayan1245@gmail.com>
2023-10-11 13:20:58 +0530

    conflict issue

--- client/src/pages/Server.jsx
+++ client/src/pages/Server.jsx
@@ -1,19 +1,13 @@
-<<<<<<< HEAD
-
-=======
-// eslint-disable-next-line no-unused-vars
-import React from 'react'
->>>>>>> 62bc5a4bd8569bce67c2972178d0bb6b9c2cc106
-import ChannelSidebar from '../features/Channels/ChannelSidebar'
-import Chat from '../features/Channels/Chat'
+import ChannelSidebar from "../features/Channels/ChannelSidebar";
+import Chat from "../features/Channels/Chat";
 
 const Server = () => {
   return (
     <div>
-       <ChannelSidebar/>
-       <Chat/>
+      <ChannelSidebar />
+      <Chat />
     </div>
-  )
-}
+  );
+};
 
-export default Server
+export default Server;
